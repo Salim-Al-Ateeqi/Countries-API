1ca78b355ab69f3c6047acb06242a13ff5f31eef
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-10-25 10:38:58 +0300

    Install Postman and continue at 32:00

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const products = require("./data");
+const products = require("./products");
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
+app.use(express.json());
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`The application is running on localhost:${PORT}`);
 });
 
 app.get("/api/products", (req, res) => {
   res.json(products);
+});
+
+app.post("/api/products", (req, res) => {
+  console.log("I'M HERE", req);
 });
