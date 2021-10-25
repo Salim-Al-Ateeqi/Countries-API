ab49f0bf02f4654c01664c58735c609a55eecbde
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-10-25 14:41:46 +0300

    Nothing works but I tried

--- App.js
+++ App.js
@@ -15,6 +15,22 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-app.post("/api/products", (req, res) => {
-  console.log("I'M HERE", req);
+app.get("/api/products/:productId", (req, res) => {
+  const product = products.find(
+    (product) => product.id === +req.params.productId
+  );
+  res.json(product);
 });
+
+app.post("/api/products", (req, res) => {
+  console.log("Your post is here:", req.body);
+  const newArray = products.push((product) => product === req.body);
+  res.json(newArray);
+});
+
+// app.delete("/api/products/:productId", (req, res) => {
+//   const product = products.find(
+//     (product) => product.id === +req.params.productId
+//   );
+//   res.json(products.filter(product.name));
+// });
